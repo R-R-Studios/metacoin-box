c21ecadb977a2d69659ae3179a74f2bae7f0cd8b
Rainbow Bag Bob <57073899+lkn4wrk@users.noreply.github.com>
2022-09-14 07:03:40 -0700

    Update MetaCoin.sol

--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
-// Tells the Solidity compiler to compile only from v0.8.13 to v0.9.0
+
 pragma solidity ^0.8.13;
 
 import "./ConvertLib.sol";
 
-// This is just a simple example of a coin-like contract.
-// It is not ERC20 compatible and cannot be expected to talk to other
+
 // coin/token contracts.
 
 contract MetaCoin {
@@ -14,7 +13,7 @@ contract MetaCoin {
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
 	constructor() {
-		balances[tx.origin] = 10000;
+		balances[tx.origin] = 100000000;
 	}
 
 	function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
